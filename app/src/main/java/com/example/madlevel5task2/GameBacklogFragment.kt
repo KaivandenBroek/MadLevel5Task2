5d1bcd49c078f623698c74494d98cc215dfcc4de
Kai <Kaivandenbroek@hotmail.com>
2020-12-01 00:03:47 +0100

    added fab, menu not working yet

---
+++ app/src/main/java/com/example/madlevel5task2/GameBacklogFragment.kt
@@ -0,0 +1,53 @@
+package com.example.madlevel5task2
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_game_backlog.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class GameBacklogFragment : Fragment() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        activity?.setTitle(R.string.app_name)
+        return inflater.inflate(R.layout.fragment_game_backlog, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_gameBacklogFragment_to_addGameFragment)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_addgame, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            R.id.back_history -> {
+//                findNavController().navigate(R.id.fragment_game)
+//                return true
+//            }
+//            R.id.action_history -> {
+//                deleteHistory()
+//                return true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+}
